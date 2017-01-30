2e9e5ac2c36ffa761bf4352c22215a40509fa198
Jared Harris <Jared Harris>
2017-01-29 23:52:57 -0600

    initial commit

---
+++ FormExample/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace FormExample
+{
+
+    public partial class Form1 : Form
+    {
+        public static Form form;
+        public static Thread thread;
+        public static int s = 100;
+        public static int s3 = 100;
+        public static int fps = 30;
+        public static double tps = 30;
+        public static double running_fps = 30.0;
+        public static double s2 = 100;
+        Box sprite = new Box();
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            form = this;
+            thread = new Thread(new ThreadStart(run));
+            thread.Start();
+        }
+
+        public static void tpsrun()
+        {
+            DateTime last = DateTime.Now;
+            DateTime now = last;
+            TimeSpan frameTime = new TimeSpan(10000000 / fps);
+            while (true)
+            {
+                DateTime temp = DateTime.Now;
+                tps = .9 * tps + .1 * 1000.0 / (temp - now).TotalMilliseconds;
+                Console.WriteLine(tps);
+                now = temp;
+                TimeSpan diff = now - last;
+                if (diff.TotalMilliseconds < frameTime.TotalMilliseconds)
+                    Thread.Sleep((frameTime - diff).Milliseconds);
+                last = DateTime.Now;
+                s3++;
+                form.Invoke(new MethodInvoker(form.Refresh));
+
+            }
+        }
+
+        public static void run()
+        {
+            DateTime last = DateTime.Now;
+            DateTime now = last;
+            TimeSpan frameTime = new TimeSpan(10000000 / fps);
+            while (true)
+            {
+                DateTime temp = DateTime.Now;
+                running_fps = .9 * running_fps + .1 * 1000.0 / (temp - now).TotalMilliseconds;
+                Console.WriteLine(running_fps);
+                now = temp;
+                TimeSpan diff = now - last;
+                if (diff.TotalMilliseconds < frameTime.TotalMilliseconds)
+                    Thread.Sleep((frameTime - diff).Milliseconds);
+                last = DateTime.Now;
+                s++;
+                s2 += 1 / running_fps;
+                form.Invoke(new MethodInvoker(form.Refresh));
+
+            }
+        }
+
+        private void UpdateSize()
+        {
+
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            thread.Abort();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            Refresh();
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '1')
+                for(int i = 0; i < 250; i++)
+                    sprite.add(new Sprite());
+            if (e.KeyChar == '0')
+                sprite.clear();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            String frames = "FPS: " + running_fps;
+            String zergcount = "Zergs: " + sprite.count();
+            String currenttps = "Ticks Per Second: " + tps;
+            e.Graphics.DrawString(frames, new Font("Ubuntu", 10), Brushes.DarkGoldenrod, 0, 0);
+            e.Graphics.DrawString(zergcount, new Font("Ubuntu", 10), Brushes.DarkGoldenrod, 200, 0);
+            e.Graphics.DrawString(currenttps, new Font("Ubuntu", 10), Brushes.DarkGoldenrod, 400, 0);
+            sprite.render(e.Graphics);
+            int v = (int)(200 + 100 * Math.Sin(s/10.0));
+            e.Graphics.DrawRectangle(Pens.Black, new Rectangle(0, 0, v, v));
+        }
+    }
+}
---
+++ FormExample/Sprite.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace FormExample
+{
+    public class Sprite
+    {
+        //instance variable
+        Image Zergling = Image.FromFile("Zergling.png");
+        private float d, r1, r2;
+        private float s = 100;
+        private float x = 0;
+
+        public Sprite()
+        {
+            Random rand = new Random();
+            d = rand.Next(1, 5);
+            r1 = rand.Next(1, 1000);
+            r2 = rand.Next(1, 500);
+        }
+
+        public float X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        private float y = 0;
+
+        public float Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        private float scale = 1;
+
+        public float Scale
+        {
+            get { return scale; }
+            set { scale = value; }
+        }
+
+        protected List<Sprite> children = new List<Sprite>();
+
+        public void clear()
+        {
+            children.Clear();
+        }
+
+        public void render(Graphics g)
+        {
+            Matrix original = g.Transform.Clone();
+            g.ScaleTransform(scale, scale);
+            g.TranslateTransform(x, y);
+            paint(g);
+            foreach (Sprite s in children)
+            {
+                s.render(g);
+            }
+            g.Transform = original;
+        }
+
+        public virtual void paint(Graphics g)
+        {
+            float v1;
+            float v2;
+            Image img;
+            v1 = (float)(r1 + r1 * Math.Tan(s / 10));
+            v2 = (float)(r2 + r2 * Math.Cos(s / 10));
+            img = Zergling;
+            g.DrawImage(img, v1, v2);
+            s++;
+        }
+
+        public void add(Sprite s)
+        {
+            children.Add(s);
+        }
+
+        public long count()
+        {
+            return children.Count();
+        }
+
+    }
+}
